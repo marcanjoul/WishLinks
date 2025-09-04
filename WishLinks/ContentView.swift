389149a7af6985ada00a10e93d76286d347e961c
Mark Anjoul <marc.anjoul@gmail.com>
2025-09-04 01:19:18 -0400

    Initial Commit

---
+++ WishLinks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WishLinks
+//
+//  Created by Mark Anjoul on 9/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WishLinks/WishLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishLinksApp.swift
+//  WishLinks
+//
+//  Created by Mark Anjoul on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WishLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
